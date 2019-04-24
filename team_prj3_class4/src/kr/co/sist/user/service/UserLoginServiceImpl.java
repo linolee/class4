003ec7d40a30a047b08bfa81a9fd5795aa76521e
linolee <43083503+linolee@users.noreply.github.com>
2019-04-24 17:23:10 +0900

    컨트롤러 정리+로그인에 쓸 DAO

--- team_prj3_class4/src/kr/co/sist/user/service/UserLoginServiceImpl.java
+++ team_prj3_class4/src/kr/co/sist/user/service/UserLoginServiceImpl.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import kr.co.sist.user.dao.LoginDAO;
 import kr.co.sist.user.dao.LoginDaoImpl;
-import kr.co.sist.user.domain.LoginDomain;
+import kr.co.sist.user.domain.Client;
 import kr.co.sist.user.vo.UserLoginVO;
 
 public class UserLoginServiceImpl implements UserLoginService{
@@ -14,20 +14,23 @@ public class UserLoginServiceImpl implements UserLoginService{
 		this.l_dao = l_dao;
 	}
 	
-	public boolean login(UserLoginVO ulvo) {
+	public String login(UserLoginVO ulvo) {
 		
 		//생성된 VO를 사용하여 DB에서 조회
-		LoginDomain ld = l_dao.selectAccount(ulvo);
+		Client ld = l_dao.selectClient(ulvo);//LoginDomain에는 id, name, status가 저장되어있다.
 		if (ld != null) {//조회된 정보가 있을 때, 로그인 정보가 맞을 때
 			//status를 확인
 			//정지/탈퇴 된 아이디일 경우 경고창으로 보내기
+			if ("".equals(ld.getStatus())) {
+				
+			}
 			//정지/탈퇴 된 아이디가 아닐 경우 세션을 생성
 		}else {//조회된 정보가 없을 때, 로그인 정보가 틀릴 때
 			//로그인 창으로 다시 보내기 
 		}
 		
 		
-		return false;
+		return "";
 	}
 	
 	//입력정보가 일치하면 세션을 생성해서 반환 
