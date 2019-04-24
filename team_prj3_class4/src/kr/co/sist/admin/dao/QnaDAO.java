d49215458e15327f0d7d93c126723a4c19ae64a2
Jaeung <didrod446@naver.com>
2019-04-24 19:30:58 +0900

    뭔가를 커밋함

---
+++ team_prj3_class4/src/kr/co/sist/admin/dao/QnaDAO.java
@@ -0,0 +1,51 @@
+package kr.co.sist.admin.dao;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.stereotype.Component;
+
+import kr.co.sist.admin.domain.QnaQuestionList;
+
+
+@Component
+public class QnaDAO {
+
+	private SqlSessionFactory ssf=null;
+	
+	public synchronized SqlSessionFactory getSessionFactory() {
+		if(ssf == null) {
+			org.apache.ibatis.logging.LogFactory.useLog4JLogging();
+			
+			Reader reader = null;
+			try {
+				//1. 설정용 xml 로딩
+				reader = Resources.getResourceAsReader("kr/co/sist/admin/dao/admin_config.xml");
+				//2. ByBatis Framwork 생성
+				SqlSessionFactoryBuilder ssfb = new SqlSessionFactoryBuilder();
+				//3. MyBatis Framework와 DB 연동한 객체 얻기 ( 객체를 하나로 관리 )
+				ssf = ssfb.build(reader);
+				if(reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}	
+		return ssf;
+	}
+
+	public List<QnaQuestionList> selectQnAQuestionList(){
+		List<QnaQuestionList> list = null;
+		
+		SqlSession ss = getSessionFactory().openSession();
+		list = ss.selectList("selectQnaQuestionList");
+		ss.close();
+		return list;
+	}
+}
---
+++ team_prj3_class4/src/kr/co/sist/admin/domain/QnaQuestionList.java
@@ -0,0 +1,38 @@
+package kr.co.sist.admin.domain;
+
+public class QnaQuestionList {
+	private String qSubject, name, qDate, aDate;
+
+	public String getqSubject() {
+		return qSubject;
+	}
+
+	public void setqSubject(String qSubject) {
+		this.qSubject = qSubject;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getqDate() {
+		return qDate;
+	}
+
+	public void setqDate(String qDate) {
+		this.qDate = qDate;
+	}
+
+	public String getaDate() {
+		return aDate;
+	}
+
+	public void setaDate(String aDate) {
+		this.aDate = aDate;
+	}
+	
+}
