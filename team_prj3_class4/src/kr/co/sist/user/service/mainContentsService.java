fbb41b0d221b639adf0e76a4867d5810f214e808
eunyoung00 <beebee6656@gmail.com>
2019-04-25 10:07:39 +0900

    MainContents

--- team_prj3_class4/src/kr/co/sist/user/service/mainContentsService.java
+++ team_prj3_class4/src/kr/co/sist/user/service/mainContentsService.java
@@ -1,6 +1,16 @@
 package kr.co.sist.user.service;
 
+import kr.co.sist.user.dao.MainContentsDAO;
+
 public class mainContentsService {
 
+	private MainContentsDAO mc_dao;
+	public mainContentsService() {
+		//mc_dao=MainContentsDAO.getInstance();
+	}//mainContentsService
+	
+//	public MainContents showContentsForm(String category) {
+//		
+//	}
 	
 }//class
