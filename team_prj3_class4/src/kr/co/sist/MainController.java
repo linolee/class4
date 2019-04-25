0836f97243bf5c4db86a182d7cbe8cfc550463ce
owner <owner@DESKTOP-L5PPD3L>
2019-04-25 10:11:32 +0900

    mainController

--- team_prj3_class4/src/kr/co/sist/MainController.java
+++ team_prj3_class4/src/kr/co/sist/MainController.java
@@ -1,11 +1,18 @@
 package kr.co.sist;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class MainController {
-	
+	@RequestMapping(value = "user/mainContents/mainContents.do", method = GET)
+	public String guestReportPage() {
+
+		return "user/mainContents/mainContents";
+	}// reportPage
 }
