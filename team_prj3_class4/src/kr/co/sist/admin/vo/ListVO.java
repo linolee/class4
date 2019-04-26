869479a9c4ebcd040758a2f6394421454fa8fc3c
Jaeung <didrod446@naver.com>
2019-04-26 17:00:32 +0900

    List 10개씩 끊고 페이지 넘어가는거 구현

---
+++ team_prj3_class4/src/kr/co/sist/admin/vo/ListVO.java
@@ -0,0 +1,30 @@
+package kr.co.sist.admin.vo;
+
+public class ListVO {
+	private int startNum, endNum, currentPage;
+
+	public int getStartNum() {
+		return startNum;
+	}
+
+	public void setStartNum(int startNum) {
+		this.startNum = startNum;
+	}
+
+	public int getEndNum() {
+		return endNum;
+	}
+
+	public void setEndNum(int endNum) {
+		this.endNum = endNum;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+	
+}
