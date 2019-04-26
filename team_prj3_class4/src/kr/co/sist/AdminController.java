364edb59fc7631f0db72919cdb07a4707469eeef
InJaEE <in11202@naver.com>
2019-04-26 19:59:52 +0900

    test2

--- team_prj3_class4/src/kr/co/sist/AdminController.java
+++ team_prj3_class4/src/kr/co/sist/AdminController.java
@@ -14,6 +14,7 @@ import kr.co.sist.admin.domain.MemberListDomain;
 import kr.co.sist.admin.domain.QnaQuestionList;
 import kr.co.sist.admin.service.MemberListService;
 import kr.co.sist.admin.service.QnaService;
+import kr.co.sist.admin.vo.ListVO;
 
 
 @Controller
@@ -26,16 +27,34 @@ public class AdminController {
 	}
 	
 	@RequestMapping(value="/admin/question.do",method=GET)
-	public String questionPage( Model model ) {
+	public String questionPage( ListVO lvo, Model model ) {
 		List<QnaQuestionList> list = null;
 		
 		//autowired로 의존성 주입//
 		ApplicationContext ac = new ClassPathXmlApplicationContext("kr/co/sist/di/ApplicationContext2.xml");
 		QnaService qs = ac.getBean(QnaService.class);
-		list = qs.selectQnAQuestionList();
 		
-		model.addAttribute("page", "question");
-		model.addAttribute("list", list);
+		int totalCount = qs.totalCount();//총 게시물의 수
+		int pageScale = qs.pageScale();
+		int totalPage = qs.totalPage(totalCount);//전체 게시물을 보여주기 위한 총 페이지 수 
+		if(lvo.getCurrentPage() == 0) { //web parameter에 값이 없을 때
+			lvo.setCurrentPage(1);
+		}
+		int startNum = qs.startNum(lvo.getCurrentPage());
+		int endNum = qs.endNum(startNum);
+
+		lvo.setStartNum(startNum);
+		lvo.setEndNum(endNum);
+
+		list = qs.selectQnAQuestionList(lvo);//리스트 목록 조회
+		String indexList = qs.indexList(lvo.getCurrentPage(), totalPage, "question.do");
+		model.addAttribute("list", list);//@@
+		model.addAttribute("indexList", indexList);
+		model.addAttribute("pageScale", pageScale);
+		model.addAttribute("totalCount", totalCount);
+		model.addAttribute("currentPage", lvo.getCurrentPage());
+
+		model.addAttribute("page", "question");//@@
 		return "admin/template";
 	}
 	
