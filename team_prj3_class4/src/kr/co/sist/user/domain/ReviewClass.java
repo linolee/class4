103198b50ec811ce01d5ab244c6e72884cd57122
eunyoung00 <beebee6656@gmail.com>
2019-04-29 14:52:22 +0900

    db연동

---
+++ team_prj3_class4/src/kr/co/sist/user/domain/ReviewClass.java
@@ -0,0 +1,37 @@
+package kr.co.sist.user.domain;
+
+public class ReviewClass {
+	private String img, tName, title, review, lcode;
+
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String gettName() {
+		return tName;
+	}
+	public void settName(String tName) {
+		this.tName = tName;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public String getLcode() {
+		return lcode;
+	}
+	public void setLcode(String lcode) {
+		this.lcode = lcode;
+	}
+	
+}
