6001b1df2049014a28a3e57268ee971455f7a7da
Kim-JungWoon <45754698+Kim-JungWoon@users.noreply.github.com>
2019-05-08 17:45:43 +0900

    ??

--- team_prj3_class4/src/kr/co/sist/user/controller/TeacherController.java
+++ team_prj3_class4/src/kr/co/sist/user/controller/TeacherController.java
@@ -31,7 +31,7 @@ public class TeacherController {
 	
 	@RequestMapping(value="user/teacher/classStatus.do", method=GET)
 	public String classStatusForm(@RequestParam(value="status", required=false) String status, HttpSession session, Model model) {
-		//session에서 아이디와 이름을 가져온다
+		//session�뿉�꽌 �븘�씠�뵒�� �씠由꾩쓣 媛��졇�삩�떎
 		String id = "";	
 		String teacherName = "";
 		id = (String)session.getAttribute("client_id");
@@ -41,20 +41,24 @@ public class TeacherController {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("kr/co/sist/di/ApplicationContextMainC.xml");
 		UserLectureService uls = ac.getBean(UserLectureService.class);
 		
-		//id와 status로 클래스 현황을 가져온다
+		if (id != null) {	
+		//id�� status濡� �겢�옒�뒪 �쁽�솴�쓣 媛��졇�삩�떎
 		List<LectureView> list = uls.searchLectureInfo(id, status);
 		
-		//아이디로 이름을 가져온다.
-		//아이디당 여러개의 프로필을 사용할 수 있으므로, List에 담는다.
+		//�븘�씠�뵒濡� �씠由꾩쓣 媛��졇�삩�떎.
+		//�븘�씠�뵒�떦 �뿬�윭媛쒖쓽 �봽濡쒗븘�쓣 �궗�슜�븷 �닔 �엳�쑝誘�濡�, List�뿉 �떞�뒗�떎.
 		List<String> tn_list = uls.searchTeacherName(id);
 		
-		//이름으로 상태별 카운트를 들고온다.
+		//�씠由꾩쑝濡� �긽�깭蹂� 移댁슫�듃瑜� �뱾怨좎삩�떎.
 		Map<String, Object> r_cnt_list = uls.searchStatusCnt(tn_list);
 		
 		model.addAttribute("l_list", list);
 		model.addAttribute("cntList", r_cnt_list);
 
 		return "user/teacher/classStatus";
+      }else {
+          return "user/member/login";		
+      } // end else
 	} //classStatusForm
 	
 
@@ -66,7 +70,7 @@ public class TeacherController {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("kr/co/sist/di/ApplicationContextMainC.xml");
 		UserLectureService uls = ac.getBean(UserLectureService.class);
 		
-		//클래스별 신청현황을 가져온다
+		//�겢�옒�뒪蹂� �떊泥��쁽�솴�쓣 媛��졇�삩�떎
 		List<LectureView> s_list = uls.searchStudentsList(code);
 		
 		return s_list;
@@ -96,35 +100,35 @@ public class TeacherController {
 		String id = "";	
 		id = (String)session.getAttribute("client_id");
 		
-		List<String> tn_list = urs.searchTeacherName(id); // id로 이름을 들고온다
+		List<String> tn_list = urs.searchTeacherName(id); // id濡� �씠由꾩쓣 �뱾怨좎삩�떎
 		
 		List<Review> r_list = new ArrayList<Review>();
 		int cnt = 0;
 		
-		//받아온 이름 수만큼 반복
+		//諛쏆븘�삩 �씠由� �닔留뚰겮 諛섎났
 		for(int i = 0; i < tn_list.size(); i++) {
 			Map<String, String> dateMap = new HashMap<String, String>();
 			dateMap.put("fromDate", fromDate);
 			dateMap.put("toDate", toDate);
 			dateMap.put("tn_list", tn_list.get(i));
 			
-			//검색값을 담은 map을 넘긴다
+			//寃��깋媛믪쓣 �떞�� map�쓣 �꽆湲대떎
 			List<Review> tmpList = urs.searchReview(dateMap);
-			if (!tmpList.isEmpty()) { //tmpList가 빈 값이 아니라면 진입
-				r_list.addAll(tmpList); // r_list에 add
-				cnt += urs.searchReviewCnt(dateMap);	//리뷰 카운트 구하기
+			if (!tmpList.isEmpty()) { //tmpList媛� 鍮� 媛믪씠 �븘�땲�씪硫� 吏꾩엯
+				r_list.addAll(tmpList); // r_list�뿉 add
+				cnt += urs.searchReviewCnt(dateMap);	//由щ럭 移댁슫�듃 援ы븯湲�
 			} // end if
 		} // end for
 		
 		//Paging
-		int pageScale = urs.pageScale(); // �� ȭ�鿡 ������ �Խù��� ��
-		int totalPage=urs.totalPage(cnt); //�� �Խù��� �����ֱ� ���� �� ������ ��
-		if(lpvo.getCurrentPage() == 0) { //web parameter�� ���� ���� ��
-			lpvo.setCurrentPage(1); //1������ ��ȸ�ؾ� �ϹǷ� 1�� ����
+		int pageScale = urs.pageScale(); // 占쏙옙 화占썽에 占쏙옙占쏙옙占쏙옙 占쌉시뱄옙占쏙옙 占쏙옙
+		int totalPage=urs.totalPage(cnt); //占쏙옙 占쌉시뱄옙占쏙옙 占쏙옙占쏙옙占쌍깍옙 占쏙옙占쏙옙 占쏙옙 占쏙옙占쏙옙占쏙옙 占쏙옙
+		if(lpvo.getCurrentPage() == 0) { //web parameter占쏙옙 占쏙옙占쏙옙 占쏙옙占쏙옙 占쏙옙
+			lpvo.setCurrentPage(1); //1占쏙옙占쏙옙占쏙옙 占쏙옙회占쌔억옙 占싹므뤄옙 1占쏙옙 占쏙옙占쏙옙
 		}//end if
 		
-		int startNum = urs.startNum(lpvo.getCurrentPage());// ���۹�ȣ
-		int endNum = urs.endNum(startNum);//����ȣ
+		int startNum = urs.startNum(lpvo.getCurrentPage());// 占쏙옙占쌜뱄옙호
+		int endNum = urs.endNum(startNum);//占쏙옙占쏙옙호
 		
 		lpvo.setStartNum(startNum);
 		lpvo.setEndNum(endNum);
@@ -163,22 +167,22 @@ public class TeacherController {
 		String id = "";	
 		id = (String)session.getAttribute("client_id");
 		
-		List<String> tn_list = uqs.searchTeacherName(id); // id로 이름들을 가져온다
+		List<String> tn_list = uqs.searchTeacherName(id); // id濡� �씠由꾨뱾�쓣 媛��졇�삩�떎
 		
 		List<Question> q_list = new ArrayList<Question>();
-		int cnt = 0;	//카운트를 담을 변수 초기화
+		int cnt = 0;	//移댁슫�듃瑜� �떞�쓣 蹂��닔 珥덇린�솕
 		
-		//teacherName수만큼 반복
+		//teacherName�닔留뚰겮 諛섎났
 		for(int i = 0; i < tn_list.size(); i++) {	
 			Map<String, String> dateMap = new HashMap<String, String>();
 			dateMap.put("fromDate", fromDate);
 			dateMap.put("toDate", toDate);
 			dateMap.put("tn_list", tn_list.get(i));
 			
-			//검색 조건값을 담은 map을 보내서 해당하는 질문글 리스트를 가져온다
+			//寃��깋 議곌굔媛믪쓣 �떞�� map�쓣 蹂대궡�꽌 �빐�떦�븯�뒗 吏덈Ц湲� 由ъ뒪�듃瑜� 媛��졇�삩�떎
 			List<Question> tmpList = uqs.searchQuestion(dateMap);
 			
-			//받아온 데이터가 존재하면 진입
+			//諛쏆븘�삩 �뜲�씠�꽣媛� 議댁옱�븯硫� 吏꾩엯
 			if (!tmpList.isEmpty()) { 
 				q_list.addAll(tmpList);
 				cnt += uqs.searchQuestionCnt(dateMap);
