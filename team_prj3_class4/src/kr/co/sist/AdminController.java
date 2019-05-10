0c0e36fa737222de7c304695a62d11a01613672c
InJaEE <in11202@naver.com>
2019-05-10 16:55:42 +0900

    메인화면

--- team_prj3_class4/src/kr/co/sist/AdminController.java
+++ team_prj3_class4/src/kr/co/sist/AdminController.java
@@ -3,18 +3,29 @@ package kr.co.sist;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.co.sist.admin.service.AdminLoginService;
+import kr.co.sist.admin.vo.StatsVO;
+
 @Controller
 public class AdminController {
 
-
+	@Autowired
+	AdminLoginService als;
+	
 	@RequestMapping(value="/admin/template.do",method= {GET,POST})
 	public String mainPage(Model model) {
 
-		return "admin/template";//
+		StatsVO svo=new StatsVO();
+		svo=als.templateStats();
+		
+		model.addAttribute("stats", svo);
+		
+		return "admin/template";
 	}
 
 }
--- team_prj3_class4/src/kr/co/sist/admin/service/AdminLoginService.java
+++ team_prj3_class4/src/kr/co/sist/admin/service/AdminLoginService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import kr.co.sist.admin.dao.AdminLoginDAO;
 import kr.co.sist.admin.vo.AdminLoginVO;
+import kr.co.sist.admin.vo.StatsVO;
 
 @Component
 public class AdminLoginService {
@@ -17,6 +18,33 @@ public class AdminLoginService {
 			flag=true;
 		}
 		return flag;
+	}
+	
+	public StatsVO templateStats(){
+		StatsVO svo=new StatsVO();
+		
+		int today=al_dao.today();
+		int month=al_dao.month();
+		
+		int totalTeacher=al_dao.totalTeacher();
+		int totalLecture=al_dao.totalLecture();
+		int ingLecture=al_dao.ingLecture();
+		int totalCategory=al_dao.totalCategory();
+		int totalClient=al_dao.totalClient();
+		int todayClient=al_dao.todayClient(today);
+		int monthClient=al_dao.monthClient(month);
+		int exitClient=al_dao.exitClient();
+		
+		svo.setTotalTeacher(totalTeacher);
+		svo.setTotalLecture(totalLecture);
+		svo.setIngLecture(ingLecture);
+		svo.setTotalCategory(totalCategory);
+		svo.setTotalClient(totalClient);
+		svo.setTodayClient(todayClient);
+		svo.setMonthClient(monthClient);
+		svo.setExitClient(exitClient);
+		
+		return svo;
 	}
 	
 }
